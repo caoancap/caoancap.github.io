60dc8a8d9d3fc3dcd2b682228f9d33d122585a31
caoancap <caoancap@users.noreply.github.com>
2026-06-22 19:28:35 +0000

    Deploying to main from @ caoancap/caoancap.github.io@ee10e387b42cf238a803ad9fce9e7a08b0e0a27c 🚀

--- _next/static/chunks/0n386o9dyrbc_.js
+++ _next/static/chunks/0n386o9dyrbc_.js
@@ -2,6 +2,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/1s1stvzgbtbc_.js",
   "static/chunks/2cbq38v59vjds.js",
   "static/chunks/35ofw5uw80608.js",
-  "static/chunks/2a0j9kj2gjbih.css",
-  "static/chunks/turbopack-1ntt_5umc9tvu.js"
+  "static/chunks/3rouphdd8bdo1.css",
+  "static/chunks/turbopack-1kiy_4sgl-wrd.js"
 ])
